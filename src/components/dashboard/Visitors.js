6b3a64dd24752f02f96570eb4d92f541104d920d
muhammadzain8 <muhammadzain8@gmail.com>
2021-10-09 17:00:14 -0700

    Visitorpage designed

--- src/components/dashboard/Visitors.js
+++ src/components/dashboard/Visitors.js
@@ -1,7 +1,253 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+
+import {
+  Typography,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableContainer,
+  TableCell,
+  TableBody,
+  Box,
+  Button,
+  TablePagination,
+  TextField,
+} from '@material-ui/core';
+import { Search as SearchIcon } from 'react-feather';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData(
+    'Muhammadzain',
+    ' zain@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadali',
+    ' ali@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadusman',
+    ' usman@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadkashif',
+    ' kashif@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadumer',
+    ' umer@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadabc',
+    ' abc@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadsonu',
+    ' sonu@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadsaqib',
+    ' saqib@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadsohil',
+    ' sohail@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadtayyab',
+    ' tayyab@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadgul',
+    'gul@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadtayyab',
+    ' tayyab@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadgul',
+    'gul@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadtayyab',
+    ' tayyab@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadgul',
+    'gul@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadtayyab',
+    ' tayyab@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadgul',
+    'gul@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadtayyab',
+    ' tayyab@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+  createData(
+    'Muhammadgul',
+    'gul@gmail.com',
+    '+2233123312334',
+    'GF12333',
+    '11/07/2021'
+  ),
+];
+
+const styles = makeStyles((theme) => ({
+  main: {
+    backgroundColor: '#f2f2f2',
+    minHeight: '20rem',
+    borderRadius: '0.8rem',
+    padding: '1rem',
+    margin: '2.5rem 1.5rem 2.5rem',
+  },
+  table: {
+    margin: ' 3rem 1rem 3rem',
+    padding: '2rem',
+    width: 'inherit',
+  },
+  textInput: {
+    width: '80%',
+    backgroundColor: '#fff',
+    marginBottom: 7,
+  },
+}));
 
 const Visitors = () => {
-  return <div>Visitor</div>;
+  const classes = styles();
+
+  return (
+    <div style={{marginTop:'5rem'}}>
+      <Typography variant='h4' m={2}>
+        {' '}
+        Customer management
+      </Typography>
+      <Box className={classes.main}>
+        <Box
+          style={{
+            display: 'flex',
+            justifyContent: 'right',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <Typography
+            variant='text'
+            style={{ margin: '0px 3px 0px' }}
+          >
+            Search Client
+          </Typography>
+          <SearchIcon style={{ margin: '0px 3px 0px' }} />
+          <TextField
+            hiddenLabel
+            id='filled-hidden-label-small'
+            defaultValue='search'
+            size='small'
+            style={{ margin: '0px 5px 0px', width: '30%' }}
+            className={classes.textInput}
+          />
+        </Box>
+        <TableContainer component={Paper} className={classes.table}>
+          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Clients</TableCell>
+                <TableCell align='right'>Emails</TableCell>
+                <TableCell align='right'>Telephone</TableCell>
+                <TableCell align='right'>Customer Ref</TableCell>
+                <TableCell align='right'>date of creation</TableCell>
+                <TableCell align='right'>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{
+                    '&:last-child td, &:last-child th': {
+                      border: 0,
+                    },
+                  }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {row.name}
+                  </TableCell>
+                  <TableCell align='right'>{row.calories}</TableCell>
+                  <TableCell align='right'>{row.fat}</TableCell>
+                  <TableCell align='right'>{row.carbs}</TableCell>
+                  <TableCell align='right'>{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </div>
+  );
 };
 
 export default Visitors;
