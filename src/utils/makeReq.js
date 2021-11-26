e5bfee64d5178f7238a6bd357e0f2a07053e6e2a
muhammadzain8 <muhammadzain8@gmail.com>
2021-11-26 12:18:37 +0500

    1 change

--- src/utils/makeReq.js
+++ src/utils/makeReq.js
@@ -8,7 +8,7 @@ import { toast } from 'react-toastify';
 
 // * Production URLs
 const API_BASE_URL = `https://goodfly-api.herokuapp.com/api`;
-// const API_BASE_ORIGIN = `https://goodfly-api.herokuapp.com`;
+const API_BASE_ORIGIN = `https://goodfly-api.herokuapp.com`;
 
 const handleCatch = (err) => {
   // console.log('**********');
