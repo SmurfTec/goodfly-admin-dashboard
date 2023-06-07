53f7d72f2ef607496e89142674ba853d3d92261d
UAhmadSoft <umadahmad1928@gmail.com>
2023-06-07 16:14:35 +0500

    Changed api uyrls

--- src/utils/makeReq.js
+++ src/utils/makeReq.js
@@ -2,16 +2,16 @@ import { LOCALSTORAGE_TOKEN_KEY } from 'Contexts/AuthContext';
 import { toast } from 'react-toastify';
 
 // * Development URLs
-const API_BASE_URL = `http://localhost:7000/api`;
-const API_BASE_ORIGIN = `http://localhost:7000`;
+// const API_BASE_URL = `http://localhost:7000/api`;
+// const API_BASE_ORIGIN = `http://localhost:7000`;
 // const API_BASE_URL = `https://c9c6-119-73-118-95.ngrok.io/api`;
 // const API_BASE_ORIGIN = `https://c9c6-119-73-118-95.ngrok.io`;
 
 // * Production URLs
-// const API_BASE_URL = `https://goodfly-api.herokuapp.com/api`;
-// const API_BASE_ORIGIN = `https://goodfly-api.herokuapp.com`;
+const API_BASE_URL = `https://goodfly-api-91tw.onrender.com/api`;
+const API_BASE_ORIGIN = `https://goodfly-api-91tw.onrender.com`;
 
-const handleCatch = (err) => {
+const handleCatch = err => {
   // console.log('**********');
   // console.log(`err`, err);
   let errMsg = 'Something Went Wrong';
@@ -36,14 +36,14 @@ const makeReq = (
     ...customConfig,
     headers: {
       ...headers,
-      ...customConfig.headers,
-    },
+      ...customConfig.headers
+    }
   };
 
   if (body) {
     config.body = JSON.stringify(body);
   }
-  return fetch(`${API_BASE_URL}${endpoint}`, config).then(async (res) => {
+  return fetch(`${API_BASE_URL}${endpoint}`, config).then(async res => {
     const data = await res.json();
     // console.log(`data`, data);
     if (res.ok) {
